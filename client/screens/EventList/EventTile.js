1c54805a33274492b3688abf6b9eeac0f3ec971c
kw1206 <kit.wallace1206@gmail.com>
2023-05-14 15:50:01 -0400

    added date formatting to eventlist page

--- client/screens/EventList/EventTile.js
+++ client/screens/EventList/EventTile.js
@@ -1,4 +1,3 @@
-//Nataly was here
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/core";
@@ -6,6 +5,65 @@ import globalStyles from "../../utils/globalStyles";
 
 const EventTile = ({ event, uid }) => {
     const navigation = useNavigation();
+
+    // function to format date/date range
+    const formatDateRange = (dateA, dateB) => {
+        let dateOrDateRange;
+        const daysOfTheWeek = [
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+        ];
+        const monthName = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+        ];
+        let convertDateA = new Date(dateA);
+        let convertDateB = new Date(dateB);
+        if (dateA === dateB) {
+            const dayOfWeek = daysOfTheWeek[convertDateA.getDay()];
+            const day = Number(dateA.slice(8, 10));
+            const month = monthName[dateA.slice(5, 7) - 1];
+            const year = Number(dateA.Aslice(0, 4));
+            dateOrDateRange = `${dayOfWeek} ${day} ${month} ${year}`;
+            return dateOrDateRange;
+        }
+
+        const startDay = daysOfTheWeek[convertDateA.getDay()];
+        const startDate = Number(dateA.slice(8, 10));
+        const startMonth = monthName[dateA.slice(5, 7) - 1];
+        const startYear = Number(dateA.slice(0, 4));
+
+        const endDay = daysOfTheWeek[convertDateB.getDay()];
+        const endDate = Number(dateB.slice(8, 10));
+        const endMonth = monthName[dateB.slice(5, 7) - 1];
+        const endYear = Number(dateB.slice(0, 4));
+
+        if (startMonth === endMonth && startYear === endYear) {
+            dateOrDateRange = `${startDay} through ${endDay}, ${startDate}-${endDate} ${startMonth} ${startYear}`;
+            return dateOrDateRange;
+        } else if (startMonth !== endMonth && startYear === endYear) {
+            dateOrDateRange = `${startDay} through ${endDay}, ${startDate} ${startMonth} - ${endDate} ${endMonth} ${startYear}`;
+            return dateOrDateRange;
+        } else if (startYear !== endYear) {
+            dateOrDateRange = `${startDay} through ${endDay}, ${startDate} ${startMonth} ${startYear} - ${endDate} ${endMonth} ${startYear}`;
+            return dateOrDateRange;
+        }
+    };
 
     return (
         <TouchableOpacity
