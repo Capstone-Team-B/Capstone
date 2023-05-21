defc099f9a3dbddd226cf990a0b9d478df9db18f
Elizabeth House <elizabeth.house88@gmail.com>
2023-05-21 16:43:04 -0400

    state on myAccount works

--- client/screens/MyAccount/MyAccountScreen.js
+++ client/screens/MyAccount/MyAccountScreen.js
@@ -5,6 +5,7 @@ import {
     SafeAreaView,
     Image,
     TouchableOpacity,
+    Pressable,
     ImageBackground,
 } from "react-native";
 import React, { useState, useEffect } from "react";
@@ -28,7 +29,6 @@ const MyAccountScreen = (props) => {
     const [user, setUser] = useState({});
     // const { uid } = props.route.params.uid;
     let isFocused = useIsFocused();
-    console.log("isFocused", isFocused);
     const [userId, setUserId] = useState("");
 
     useEffect(() => {
@@ -45,21 +45,20 @@ const MyAccountScreen = (props) => {
             if (snapshot.exists()) {
                 const data = Object.keys(snapshot.val());
                 setUserId(data[0]);
+                get(child(dbRef, `users/${data[0]}`))
+                    .then((snapshot) => {
+                        if (snapshot.exists()) {
+                            setUser(snapshot.val());
+                        } else {
+                            console.log("No data available");
+                        }
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
             }
         };
         getUserId();
-        const dbRef = ref(getDatabase());
-        get(child(dbRef, `users/${userId}`))
-            .then((snapshot) => {
-                if (snapshot.exists()) {
-                    setUser(snapshot.val());
-                } else {
-                    console.log("No data available");
-                }
-            })
-            .catch((error) => {
-                console.log(error);
-            });
     }, [isFocused]);
 
     const navigation = useNavigation();
